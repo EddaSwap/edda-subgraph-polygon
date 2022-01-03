7f8b900560c3398e94b7612932cb563abc16a644
Le Trung Kien <akkien99@gmail.com>
2022-01-03 22:07:24 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Eddaswap Subgraph
+[Eddaswap](https://eddaswap.com/) EDDASwap is an ecosystem of DeFi + NFT applications with a DEX supporting Polygon, Ethereum, Binance Smart Chain and Polkadot.
+This subgraph is forked from Uniswap V2.
+
---
+++ src/mappings/pricing.ts
@@ -0,0 +1,139 @@
+/* eslint-disable prefer-const */
+import { Pair, Token, Bundle } from '../types/schema'
+import { BigDecimal, Address, BigInt } from '@graphprotocol/graph-ts/index'
+import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD, UNTRACKED_PAIRS } from './helpers'
+
+let WBNB_ADDRESS = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+let USDT_WBNB_PAIR = '0xa84221ad67a996d6753c1456b7a2711084fb75ea'
+
+export function getBnbPriceInUSD(): BigDecimal {
+  // fetch eth prices for each stablecoin
+  let usdtPair = Pair.load(USDT_WBNB_PAIR) // USDT is token1
+  if (usdtPair !== null) {
+    return usdtPair.token1Price
+  } else {
+    return ZERO_BD
+  }
+}
+
+// token where amounts should contribute to tracked volume and liquidity
+let WHITELIST: string[] = [
+  '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', // WMATIC
+  '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063', // DAI
+  '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', // USDT
+  '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', // USDC
+  '0xdf7837de1f2fa4631d716cf2502f8b230f1dcc32', // TEL
+  '0x8505b9d2254a7ae468c0e9dd10ccea3a837aef5c', // COMP
+  '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', // WETH
+  '0xb8cb8a7f4c2885c03e57e973c74827909fdc2032', // YFI
+  '0xb33eaad8d922b1083446dc23f610c2567fb5180f', // UNI
+  '0xd6df932a45c0f255f85145f286ea0b292b21c90b', // AAVE
+  '0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39', // LINK
+  '0xfbdd194376de19a88118e84e279b977f165d01b8' // BIFI
+]
+
+// minimum liquidity required to count towards tracked volume for pairs with small # of Lps
+let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('1000')
+
+// minimum liquidity for price to get tracked
+let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('1')
+
+/**
+ * Search through graph to find derived Eth per token.
+ * @todo update to be derived ETH (add stablecoin estimates)
+ **/
+export function findEthPerToken(token: Token): BigDecimal {
+  if (token.id == WBNB_ADDRESS) {
+    return ONE_BD
+  }
+  // loop through whitelist and check if paired with any
+  for (let i = 0; i < WHITELIST.length; ++i) {
+    let pairAddress = factoryContract.getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]))
+    if (pairAddress.toHexString() != ADDRESS_ZERO) {
+      let pair = Pair.load(pairAddress.toHexString())
+      if (pair.token0 == token.id && pair.reserveETH.gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
+        let token1 = Token.load(pair.token1)
+        return pair.token1Price.times(token1.derivedETH as BigDecimal) // return token1 per our token * Eth per token 1
+      }
+      if (pair.token1 == token.id && pair.reserveETH.gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
+        let token0 = Token.load(pair.token0)
+        return pair.token0Price.times(token0.derivedETH as BigDecimal) // return token0 per our token * ETH per token 0
+      }
+    }
+  }
+  return ZERO_BD // nothing was found return 0
+}
+
+/**
+ * Accepts tokens and amounts, return tracked amount based on token whitelist
+ * If one token on whitelist, return amount in that token converted to USD.
+ * If both are, return average of two amounts
+ * If neither is, return 0
+ */
+export function getTrackedVolumeUSD(
+  tokenAmount0: BigDecimal,
+  token0: Token,
+  tokenAmount1: BigDecimal,
+  token1: Token,
+  pair: Pair
+): BigDecimal {
+  let bundle = Bundle.load('1')
+  let price0 = token0.derivedETH.times(bundle.ethPrice)
+  let price1 = token1.derivedETH.times(bundle.ethPrice)
+
+  // both are whitelist tokens, take average of both amounts
+  if (WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+    return tokenAmount0
+      .times(price0)
+      .plus(tokenAmount1.times(price1))
+      .div(BigDecimal.fromString('2'))
+  }
+
+  // take full value of the whitelisted token amount
+  if (WHITELIST.includes(token0.id) && !WHITELIST.includes(token1.id)) {
+    return tokenAmount0.times(price0)
+  }
+
+  // take full value of the whitelisted token amount
+  if (!WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+    return tokenAmount1.times(price1)
+  }
+
+  // neither token is on white list, tracked volume is 0
+  return ZERO_BD
+}
+
+/**
+ * Accepts tokens and amounts, return tracked amount based on token whitelist
+ * If one token on whitelist, return amount in that token converted to USD * 2.
+ * If both are, return sum of two amounts
+ * If neither is, return 0
+ */
+export function getTrackedLiquidityUSD(
+  tokenAmount0: BigDecimal,
+  token0: Token,
+  tokenAmount1: BigDecimal,
+  token1: Token
+): BigDecimal {
+  let bundle = Bundle.load('1')
+  let price0 = token0.derivedETH.times(bundle.ethPrice)
+  let price1 = token1.derivedETH.times(bundle.ethPrice)
+
+  // both are whitelist tokens, take average of both amounts
+  if (WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+    return tokenAmount0.times(price0).plus(tokenAmount1.times(price1))
+  }
+
+  // take double value of the whitelisted token amount
+  if (WHITELIST.includes(token0.id) && !WHITELIST.includes(token1.id)) {
+    return tokenAmount0.times(price0).times(BigDecimal.fromString('2'))
+  }
+
+  // take double value of the whitelisted token amount
+  if (!WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+    return tokenAmount1.times(price1).times(BigDecimal.fromString('2'))
+  }
+
+  // neither token is on white list, tracked volume is 0
+  return ZERO_BD
+}
